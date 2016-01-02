de2cfe03984bebf85082bff0ba881793b8c01b63
Alexandre Gama <login+alexandre.gama@elo7.com>
2016-01-02 10:16:50 -0200

    Add initial structure

---
+++ src/main/java/com/bookmark/controller/GreetingController.java
@@ -0,0 +1,19 @@
+package com.bookmark.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class GreetingController {
+
+	@RequestMapping(value = "/greetings", method = RequestMethod.GET)
+	public String hello(@RequestParam(name = "name", required = false) String name, Model model) {
+		model.addAttribute("name", name);
+
+		return "greetings";
+	}
+
+}
